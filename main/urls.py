58a0947dcf4cf13510be05ed3bc462d3f1c41e14
Пётр Иванов13 <Ivanov13@example.com>
2024-09-12 23:44:08 +0300

    The final view of the site has been formed, and information pages have been made

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
 
